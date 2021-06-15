6ddc59aa4f5fabf971b21f11dca728848bbdf0d3
Anders Nilsson <nilssonanders79@gmail.com>
2021-06-15 14:03:24 +0200

    catch any error
    if an input e.g. "github_token" was not set the code failed with an unhandled promise rejection but the action was considered to be successful

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const github = require('@actions/github')
 const core = require('@actions/core');
 
 async function run() {
-    const token = core.getInput('github_token')
-    const target_branch = core.getInput('target_branch')
-    const source_ref = core.getInput('source_ref')
-    const commit_message_template = core.getInput('commit_message_template')
-    const octokit = github.getOctokit(token);
-
-    const repo = github.context.repo
-
     try {
+        const token = core.getInput('github_token')
+        const target_branch = core.getInput('target_branch')
+        const source_ref = core.getInput('source_ref')
+        const commit_message_template = core.getInput('commit_message_template')
+        const octokit = github.getOctokit(token);
+
+        const repo = github.context.repo
 
         let commitMessage = commit_message_template
             .replace('{source_ref}', source_ref)
